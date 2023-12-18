1467742ff9e46fea67b9e88eb026450166e1abb0
TruePacifism <gorov2323@mail.ru>
2023-12-18 15:56:26 +0300

    Убрал варнинги

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,12 +1,9 @@
-import React, { Dispatch, SetStateAction, useState } from "react";
+import React from "react";
 import styles from "./Header.module.css";
 import { ReactComponent as BackIcon } from "../../images/back-icon.svg";
 import { ReactComponent as LogoIcon } from "../../images/logo-another-icon.svg";
 import { ReactComponent as InfoIcon } from "../../images/info-icon.svg";
 import { Link, useLocation } from "react-router-dom";
-import { ThemeProvider } from "@emotion/react";
-import { searchModalTheme } from "../../utils/muiThemes";
-import { Dialog, Input } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { stateType } from "../../utils/types";
 import { closeModalAction, openModalAction } from "../../utils/store";
@@ -17,13 +14,10 @@ type propsType = {
 
 export default function Header({ heading }: propsType) {
   const dispatch = useDispatch();
-  const [isSearching, setIsSearching]: [
-    boolean,
-    Dispatch<SetStateAction<boolean>>
-  ] = useState(false);
   const isErrorPage = useSelector<stateType, boolean>(
     (state) => state.isErrorPage
   );
+  // eslint-disable-next-line
   const closeModal = () => {
     dispatch(closeModalAction());
   };
@@ -40,18 +34,6 @@ export default function Header({ heading }: propsType) {
   return (
     !isErrorPage && (
       <>
-        <ThemeProvider theme={searchModalTheme}>
-          <Dialog
-            style={{ backgroundColor: "transparent !important" }}
-            open={isSearching}
-            onClose={closeModal}
-          >
-            <div className={styles.modalContainer}>
-              <InfoIcon className={styles.searchModalIcon} />
-              <Input />
-            </div>
-          </Dialog>
-        </ThemeProvider>
         <div className={styles.container}>
           {heading === "СИСТЕМА ФЕНШУЙ" ? (
             <InfoIcon className={styles.backIcon} onClick={openModal} />
