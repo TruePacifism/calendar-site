bb99086d275552696cb5cb8a1a9d7af61461bc4d
TruePacifism <76737760+TruePacifism@users.noreply.github.com>
2024-01-02 16:49:17 +0300

    Перенос хостинга

--- src/api/apiVars.ts
+++ src/api/apiVars.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // const PORT = 3333;
 // export const uri = `http://localhost:${PORT}`;
-export const uri = "https://calendar-2vknvhi8.b4a.run";
+export const uri = "https://2355021-yo82697.twc1.net";
 export const apiClient = axios.create({ baseURL: uri });
