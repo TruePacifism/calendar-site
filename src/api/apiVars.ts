b683c426e8d5ab03c057897b4199a18c54c472c7
TruePacifism <76737760+TruePacifism@users.noreply.github.com>
2024-01-05 20:47:03 +0300

    Update apiVars.ts

--- src/api/apiVars.ts
+++ src/api/apiVars.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // const PORT = 3333;
 // export const uri = `http://localhost:${PORT}`;
-export const uri = "http://89.23.116.112:3333";
+export const uri = "http://45.8.99.86:443";
 export const apiClient = axios.create({ baseURL: uri });
