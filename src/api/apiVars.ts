9facce8aed975f92c0e542542f49b5340278f5c9
TruePacifism <gorov2323@mail.ru>
2024-01-21 17:19:32 +0300

    Изменил порт

--- src/api/apiVars.ts
+++ src/api/apiVars.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const PORT = 443;
+const PORT = 3000;
 // export const uri = `http://localhost:${PORT}`;
 export const uri = `http://45.8.99.86:${PORT}`;
 export const apiClient = axios.create({ baseURL: uri });
