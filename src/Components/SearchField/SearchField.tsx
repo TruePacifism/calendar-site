d79499406e792e15ba423aaa0f09d746d5210387
TruePacifism <gorov2323@mail.ru>
2024-01-28 20:39:03 +0300

    Сделал полностью рабочий поиск

--- src/Components/SearchField/SearchField.tsx
+++ src/Components/SearchField/SearchField.tsx
@@ -1,29 +1,59 @@
-import React, { useCallback, useRef } from "react";
+import React, {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import styles from "./SearchField.module.css";
 import { ReactComponent as SearchIcon } from "../../images/search-icon.svg";
 import { useDispatch, useSelector } from "react-redux";
 import { openModalAction } from "../../utils/store";
-import { useAutocomplete } from "@mui/material";
+import { Input, useAutocomplete } from "@mui/material";
 import { cardInfoType, stateType } from "../../utils/types";
 
-export default function SearchField() {
-  const dispatch = useDispatch();
+function SearchFieldModal() {
   const cards = useSelector<stateType, cardInfoType[]>(
     (store) => store.user.cards
   );
-  const {
-    getRootProps,
-    getInputLabelProps,
-    getInputProps,
-    getListboxProps,
-    getOptionProps,
-    groupedOptions,
-  } = useAutocomplete({
-    id: "use-autocomplete-demo",
-    options: cards,
-    getOptionLabel: (cards) => cards.name,
-  });
+  const [searchValue, setSearchValue]: [
+    string,
+    Dispatch<SetStateAction<string>>
+  ] = useState("");
+  useEffect(() => {
+    console.log(searchValue);
+  }, [searchValue]);
+  return (
+    <>
+      <div className={styles.searchContainer}>
+        <SearchIcon className={styles.icon} />
+        <input
+          className={styles.placeholder}
+          value={searchValue}
+          onChange={(e) => {
+            console.log(e);
+            setSearchValue(e.target.value);
+          }}
+          placeholder="найти карту"
+        />
+      </div>
+      <ul className={styles.searchResultList}>
+        {cards &&
+          cards
+            .filter((card) =>
+              card.name.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((card) => (
+              <li className={styles.searchResultItem}>{card.name}</li>
+            ))}
+      </ul>
+    </>
+  );
+}
 
+export default function SearchField() {
+  const dispatch = useDispatch();
   const searchRef = useRef<HTMLDivElement>(null);
   const fetchClickSearch = useCallback(() => {
     if (!searchRef.current) {
@@ -31,73 +61,25 @@ export default function SearchField() {
     }
     dispatch(
       openModalAction(
-        <div>
-          <div {...getRootProps()}>
-            <label {...getInputLabelProps()}>useAutocomplete</label>
-            <input {...getInputProps()} />
-          </div>
-          {groupedOptions.length > 0 ? (
-            <ul
-              style={{
-                width: 200,
-                margin: 0,
-                padding: 0,
-                zIndex: 1,
-                position: "absolute",
-                listStyle: "none",
-                backgroundColor: "#fff",
-                overflow: "auto",
-                maxHeight: 200,
-                border: "1px solid rgba(0,0,0,.25)",
-                // "& li.Mui-focused": {
-                //   backgroundColor: "#4a8df6",
-                //   color: "white",
-                //   cursor: "pointer",
-                // },
-                // "& li:active": {
-                //   backgroundColor: "#2977f5",
-                //   color: "white",
-                // },
-              }}
-              {...getListboxProps()}
-            >
-              {(groupedOptions as typeof cards).map((option, index) => (
-                <li {...getOptionProps({ option, index })}>{option.name}</li>
-              ))}
-            </ul>
-          ) : null}
+        <div
+          className={styles.container}
+          style={{
+            position: "absolute",
+            top: searchRef.current.getBoundingClientRect().top,
+            left: 13,
+            width: "calc(100% - 13px*2)",
+          }}
+        >
+          <SearchFieldModal />
         </div>
       )
     );
-  }, [
-    dispatch,
-    getRootProps,
-    getInputLabelProps,
-    getInputProps,
-    getListboxProps,
-    getOptionProps,
-    groupedOptions,
-  ]);
-  // <div
-  //   className={styles.container}
-  //   style={{
-  //     position: "absolute",
-  //     top: searchRef.current.getBoundingClientRect().top,
-  //     left: 26,
-  //     width:
-  //       window.innerWidth > 400
-  //         ? 400 - 26 * 2
-  //         : (window.innerWidth - 400) / 2 - 26 * 2,
-  //   }}
-  // >
-  //   <SearchIcon className={styles.icon} />
-  //   <span className={styles.placeholder}>Найти карту</span>
-  // </div>
+  }, [dispatch]);
 
   return (
     <div
       ref={searchRef}
-      className={styles.container}
+      className={styles.searchContainer}
       onClick={fetchClickSearch}
     >
       <SearchIcon className={styles.icon} />
