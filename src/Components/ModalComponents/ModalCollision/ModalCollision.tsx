a709093c5106d8bccca6cf16e3753c48d9bf43f0
TruePacifism <76737760+TruePacifism@users.noreply.github.com>
2024-01-30 19:54:22 +0300

    Новые сочетания и цвет

--- src/Components/ModalComponents/ModalCollision/ModalCollision.tsx
+++ src/Components/ModalComponents/ModalCollision/ModalCollision.tsx
@@ -3,59 +3,51 @@ import styles from "./ModalCollision.module.css";
 import { ReactComponent as Rectangle } from "../../../images/collisions/rectangle.svg";
 import { ReactComponent as Circle } from "../../../images/collisions/circle.svg";
 import { ReactComponent as Heart } from "../../../images/collisions/heart.svg";
-import { ReactComponent as Cloud } from "../../../images/collisions/cloud.svg";
+import { ReactComponent as Half } from "../../../images/collisions/half-circle.svg";
+import { ReactComponent as Triangle } from "../../../images/collisions/triangle.svg";
+import { ReactComponent as HalfHorizontal } from "../../../images/collisions/half-circle-horizontal.svg";
 import { collisionType } from "../../../utils/types";
 
 type propsType = {
   collision: collisionType;
 };
 
-const getShape = (shape: string, color: string) => {
+const getShape = (shape: string, color: string, secondColor?: string) => {
+  const className = [styles.shape, getColorClassName(color)].join(" ");
   switch (shape) {
     case "half":
-      return <Cloud className={getColorClassName(color)} />;
+      return (
+        <Half
+          className={[
+            styles.shape,
+            getColorClassName(color, "-left"),
+            getColorClassName(secondColor ? secondColor : color, "-right"),
+          ].join(" ")}
+        />
+      );
     case "circle":
-      return <Circle className={getColorClassName(color)} />;
+      return <Circle className={className} />;
     case "rectangle":
-      return <Rectangle className={getColorClassName(color)} />;
+      return <Rectangle className={className} />;
     case "heart":
-      return <Heart className={getColorClassName(color)} />;
+      return <Heart className={className} />;
+    case "triangle":
+      return <Triangle className={className} />;
+    case "half-horizontal":
+      return <HalfHorizontal className={className} />;
     default:
-      return <Heart className={getColorClassName(color)} />;
+      return <Heart className={className} />;
   }
 };
 
-const getColorClassName = (color: string) => {
-  switch (color) {
-    case "red":
-      return styles.red;
-    case "lightGreen":
-      return styles.lightGreen;
-    case "lightBlue":
-      return styles.lightBlue;
-    case "blue":
-      return styles.darkBlue;
-    case "brown":
-      return styles.brown;
-    case "orange":
-      return styles.orange;
-    case "pink":
-      return styles.pink;
-    case "purple":
-      return styles.purple;
-    case "darkGreen":
-      return styles.darkGreen;
-    case "yellow":
-      return styles.yellow;
-    default:
-      break;
-  }
+const getColorClassName = (color: string, postfix?: string) => {
+  return styles[color + (postfix ? postfix : "")];
 };
 
 export default function ModalCollision({ collision }: propsType) {
   return (
     <li className={styles.container}>
-      {getShape(collision.shape, collision.color)}
+      {getShape(collision.shape, collision.color, collision.secondColor)}
       <p className={styles.description}>{`${collision.kind}: ${
         collision.animal.name
       } - ${collision.secondTarget.animal.name} ${
